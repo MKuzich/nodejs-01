a7ae57c55716656b7cf8563cd6a0278d8b012c71
Mykhailo Kuzich <timmkedth@gmail.com>
2022-10-09 12:55:46 +0300

    try catch added

--- contacts.js
+++ contacts.js
@@ -4,39 +4,55 @@ const path = require("path");
 const contactsPath = path.resolve("./db/contacts.json");
 
 async function listContacts() {
-  const data = await fs.readFile(contactsPath);
-  const parsedData = JSON.parse(data);
-  console.log(parsedData);
-  return parsedData;
+  try {
+    const data = await fs.readFile(contactsPath);
+    const parsedData = JSON.parse(data);
+    console.log(parsedData);
+    return parsedData;
+  } catch (err) {
+    console.error(err);
+  }
 }
 
 async function getContactById(contactId) {
-  const data = await fs.readFile(contactsPath);
-  const parsedData = JSON.parse(data);
-  const contactData = parsedData.find((item) => item.id === contactId);
-  console.log(contactData);
+  try {
+    const data = await fs.readFile(contactsPath);
+    const parsedData = JSON.parse(data);
+    const contactData = parsedData.find((item) => item.id === contactId);
+    console.log(contactData);
+  } catch (err) {
+    console.error(err);
+  }
 }
 
 async function removeContact(contactId) {
-  const data = await fs.readFile(contactsPath);
-  const parsedData = JSON.parse(data);
-  const newData = JSON.stringify(
-    parsedData.filter((item) => item.id !== contactId)
-  );
-  fs.writeFile(contactsPath, newData);
-  console.log(`Contact with id ${contactId} deleted succesfull!`);
+  try {
+    const data = await fs.readFile(contactsPath);
+    const parsedData = JSON.parse(data);
+    const newData = JSON.stringify(
+      parsedData.filter((item) => item.id !== contactId)
+    );
+    fs.writeFile(contactsPath, newData);
+    console.log(`Contact with id ${contactId} deleted succesfull!`);
+  } catch (err) {
+    console.error(err);
+  }
 }
 
 async function addContact(name, email, phone) {
-  const data = await fs.readFile(contactsPath);
-  const parsedData = JSON.parse(data);
-  const newId = (Number(parsedData[parsedData.length - 1].id) + 1).toString();
-  const newData = JSON.stringify([
-    ...parsedData,
-    { id: newId, name, email, phone },
-  ]);
-  fs.writeFile(contactsPath, newData);
-  console.log(`Contact ${name} added succesfull!`);
+  try {
+    const data = await fs.readFile(contactsPath);
+    const parsedData = JSON.parse(data);
+    const newId = (Number(parsedData[parsedData.length - 1].id) + 1).toString();
+    const newData = JSON.stringify([
+      ...parsedData,
+      { id: newId, name, email, phone },
+    ]);
+    fs.writeFile(contactsPath, newData);
+    console.log(`Contact ${name} added succesfull!`);
+  } catch (err) {
+    console.error(err);
+  }
 }
 
 module.exports = {
